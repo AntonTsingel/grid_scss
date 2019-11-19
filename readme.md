655eb644b94ba3d1ef83220201915a7533d6fedc
Anton Tsingel <atsingel@VRP.local>
2019-11-19 17:15:36 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+# GRID SCSS 
+
+Project related to flex grid implementations using scss.
+
+## Getting Started
+
+After addind all files write in cmd :
+```
+npm i
+```
+this will install the necessary modules 
+
+And run project with command using npm script "start"
+```
+npm start
+```
+Project will be open on port 8000.
---
+++ server.js
@@ -0,0 +1,11 @@
+var static = require('node-static');
+
+var file = new static.Server('./');
+ 
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+
+        file.serve(request, response);
+    }).resume();
+}).listen(8000);
+console.log('server available on: http://localhost:8000');
